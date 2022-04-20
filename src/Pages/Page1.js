6d6ff8db960a3bfd00c83f4c176ca6c980a151ae
Prashant Bhat <prashantbhat84@gmail.com>
2022-04-20 19:16:21 +0530

    test123

---
+++ src/Pages/Page1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page1 = () => {
+    return (
+
+        <h2>
+            This is the first page
+        </h2>
+
+    )
+}
+
+export default Page1
---
+++ src/Pages/Page2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page2 = () => {
+    return (
+
+        <h2>
+            This is the second pagepage
+        </h2>
+
+    )
+}
+
+export default Page2
